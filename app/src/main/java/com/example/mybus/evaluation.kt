71a4e07b239d2bd7ca490dd404cf732585fc1be4
banan <banangh1418@gmail.com>
2020-02-24 10:26:31 +0300

    test

---
+++ app/src/main/java/com/example/mybus/evaluation.kt
@@ -0,0 +1,12 @@
+package com.example.mybus
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class evaluation : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_evaluation)
+    }
+}
---
+++ app/src/main/java/com/example/mybus/payment.kt
@@ -0,0 +1,12 @@
+package com.example.mybus
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class payment : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_payment)
+    }
+}
